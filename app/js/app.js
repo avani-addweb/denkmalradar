21878d2beaaa0f8e39a161791b51d8b2d3fa38f4
hiral-addweb <hiral.addweb@gmail.com>
2018-07-04 11:32:47 +0530

    Added design of slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,4 @@
 jQuery(document).ready(function(){
 
-  // alert('hi');
 
-  jQuery('.search-view').click(function(){
-    // alert('hi');
-    jQuery('.Grid-view-card').slideToggle();
-    jQuery('.list-view-card').slideToggle();
-  });
-
-  jQuery('select').selectric();
 });
