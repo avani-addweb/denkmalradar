cc51db6ae8cb4030e71f5b6b6cf650dc3129370a
hiral-addweb <hiral.addweb@gmail.com>
2018-07-05 17:13:38 +0530

    Added HTML structure of search page

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function(){
 
+  // Suchen page tab layout js
+  jQuery('.layout-tabs li').click(function() {
+    var target = "#" + this.getAttribute('data-target');
+    jQuery('.layout-tabs li').removeClass('active');
+    jQuery('.layout-view').removeClass('active');
+    jQuery(this).addClass('active');
+    jQuery('body').find(target).addClass('active');
+  });
 
 });
